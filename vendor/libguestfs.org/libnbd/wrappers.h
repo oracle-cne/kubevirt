01ad219f75bb8fbaf7c7d474f385827b82ee695f
Oracle Cloud Native Environment Authors <noreply@oracle.com>
2026-03-25 06:16:06 +0000

    Updated vendor folder

--- vendor/libguestfs.org/libnbd/wrappers.h
+++ vendor/libguestfs.org/libnbd/wrappers.h
@@ -30,21 +30,6 @@
 
 #include "libnbd.h"
 
-#ifndef LIBNBD_HAVE_NBD_BLOCK_STATUS_64
-typedef struct nbd_extent {
-  uint64_t length;
-  uint32_t flags;
-} nbd_extent;
-
-typedef struct {
-  int (*callback) (void *user_data, const char *metacontext,
-                   uint64_t offset, nbd_extent *entries,
-                   size_t nr_entries, int *error);
-  void (*free) (void *user_data);
-  void *user_data;
-} nbd_extent64_callback;
-#endif
-
 /* When calling callbacks we pass the callback ID (a golang int /
  * C.long) in the void *user_data field.  We need to create a block
  * to store the callback number.  This must be freed by C.free(vp)
