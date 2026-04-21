ff75c7eeb4ca20cea5165af1d4bb09b7c0fedd84
Oracle Cloud Native Environment Authors <noreply@oracle.com>
2026-04-21 11:49:53 +0000

    vendor/libnbd: add fallback extent callback typedefs
    Define `nbd_extent` and `nbd_extent64_callback` when older libnbd
    headers do not provide them, fixing `virt-launcher` builds in RPM
    environments with mismatched system headers.

--- vendor/libguestfs.org/libnbd/wrappers.h
+++ vendor/libguestfs.org/libnbd/wrappers.h
@@ -30,6 +30,24 @@
 
 #include "libnbd.h"
 
+#if !defined(LIBNBD_HAVE_NBD_BLOCK_STATUS_64) && \
+    !defined(LIBNBD_HAVE_NBD_BLOCK_STATUS_FILTER) && \
+    !defined(LIBNBD_HAVE_NBD_AIO_BLOCK_STATUS_64) && \
+    !defined(LIBNBD_HAVE_NBD_AIO_BLOCK_STATUS_FILTER)
+typedef struct {
+  uint64_t length;
+  uint32_t flags;
+} nbd_extent;
+
+typedef struct {
+  int (*callback)(void *user_data, const char *metacontext,
+                  uint64_t offset, nbd_extent *entries,
+                  size_t nr_entries, int *error);
+  void (*free)(void *user_data);
+  void *user_data;
+} nbd_extent64_callback;
+#endif
+
 /* When calling callbacks we pass the callback ID (a golang int /
  * C.long) in the void *user_data field.  We need to create a block
  * to store the callback number.  This must be freed by C.free(vp)
